15ac5487e13e5dc63d9174ccf52adfbaf6b6c493
Kaisma Penn-Titley <kptitley@gmail.com>
2017-07-30 16:08:15 -0400

    updated html and js files before debugging and adding css

--- listingBooks.js
+++ listingBooks.js
@@ -1,7 +1,63 @@
-var authorName = document.getElementById("author-name"), 
+var authorFirstName = document.getElementById("author-first-name"), 
+    authorLastName = document.getElementById("author-last-name"),
     bookTitle = document.getElementById("book-title"),
     addAuthorButton = document.getElementById("add-author-button"),
     resetAuthorsButton = document.getElementById("reset-authors-button"),
-    displayDiv = document.getElementById("display-div"); 
+    displayDiv = document.getElementById("display-div"),
+    authorList = [];
 
-    alert("JS works on this page");
+    //SCOPE OF APPLICATION
+        //Once addAuthorButton is clicked, will like to:
+            //1. Use a constructor to create an object about author & book title
+            //2. Add the author details to an array via push()
+            //3. Loop through array to verify that all authors and book titles are entered
+            //4. Display results to the webpage via displayDiv
+            //5. As a bonus, be able to clear all information being displayed via the resetAuthorsButton 
+
+    //will construct an object with author name & book title
+        //aFName, aLName & aBookTitle are parameteres or placeholders for what will be passed to the function when it's called
+    var Author = function Author(aFName, aLName, aBookTitle) { 
+         this.aFirstName = authorFirstName;
+         this.aLastName = authorLastName;
+         this.bookTitle = bookTitle;
+         
+         //will convert the index of each author and corresponding book (array data), into a string for displaying to the user
+         this.toString = function toString() {
+         return this.authorFirstName + " " + this.authorLastName + "\n" + "Title of Book: " + this.bookTitle;
+        }
+    }
+
+    //will call on the Author constructor to create new object for each new author,
+        //push author information to authorList array & update the array
+    addAuthorButton.addEventListener("click", function() {
+        var newlyCreatedAuthor = new Author(authorFirstName.value, authorLastName.value, bookTitle.value)
+            authorList.push(newlyCreatedAuthor); //JS interpereter will handle layout and formatting once object is added to the list
+            updateArrayList();
+    });
+
+    //will add author info to array via push(), loop through the array
+        //and update displayDiv by clearing info that's present in preparation
+        //to post updated array
+    var updateArrayList = function updateArrayList() {
+        displayDiv.innerHTML = "";
+
+        for(i = 0; i < authorList.length; i++) {
+            var newAuthorInfo = authorList[i];
+            displayDiv.innerText += i + ": " + newAuthorInfo.toString() + "\n";
+        } 
+    }
+
+    //designed to clear <divs> used as display areas and text boxes used for input
+        //& will only execute once called
+    var clearDiv = function clearDiv() {
+        displayDiv.innerHTML = ""
+        authorFirstName.value = "";
+        authorLastName.value = "";
+    }
+
+    //reset authors button
+    resetAuthorsButton.addEventListener("click", function() {
+        clearDiv();
+    });
+
+
