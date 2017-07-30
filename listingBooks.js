db16673105e55e337dd3b947f88be5b47b690bde
Kaisma Penn-Titley <kptitley@gmail.com>
2017-07-29 22:25:10 -0400

    initial JS and HTML for Book listing web app

---
+++ listingBooks.js
@@ -0,0 +1,7 @@
+var authorName = document.getElementById("author-name"), 
+    bookTitle = document.getElementById("book-title"),
+    addAuthorButton = document.getElementById("add-author-button"),
+    resetAuthorsButton = document.getElementById("reset-authors-button"),
+    displayDiv = document.getElementById("display-div"); 
+
+    alert("JS works on this page");
